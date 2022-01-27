525585f59241c8170fc867f79157d4733bcc5175
Debmalya Pramanik <DebmalyaPramanik.005@gmail.com>
2022-01-27 13:17:54 +0530

    bump snake game 1.0.0
    * update to use engine for n-number of games
    * rename main.py to snake.py
    * update documentations

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,11 +1,15 @@
 # -*- encoding: utf-8 -*-
 
 """
-Game Engine Module - Implement Snake Game using PyGame
+DeepQ Learning is a Reinforcement Learning Platform where AI Learns to Play Games
 
-The `engine` is developed such that the snake game can be built,
-tested and played seperately from the rest of the application. The
-model is built with `pygame`.
+In this project, I'm trying to develop some 'Q-Learning Algorithms' where the
+neural network will learn to play various games. The `engine` is specifically
+designed to build games that can be used to train and test the models. The game
+engines are also built such that an user can self play without any overhead.
+
+List of Games Available:
+  1. Classic Snake Game (`snake.py`)
 
 @author:  Debmalya Pramanik
 @Contact: dPramanik.official@gmail.com
