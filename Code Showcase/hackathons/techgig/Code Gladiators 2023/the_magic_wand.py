0a94e974299ff25d57a1a4afdbb8b64ddb5cc921
ZenithClown <debmalyapramanik.005@gmail.com>
2023-04-17 11:42:39 +0530

    Revert "🚧💣 dictionary based calculation passed 2/11"
    This reverts commit 82474d3c2cac8afb813f836a72cd06c6a8d4dc33.

--- Code Showcase/hackathons/techgig/Code Gladiators 2023/the_magic_wand.py
+++ Code Showcase/hackathons/techgig/Code Gladiators 2023/the_magic_wand.py
@@ -1,16 +1,11 @@
 # -*- encoding: utf-8 -*-
 
-counter = lambda li : {k : li.count(k) for k in set(A)}
-# cum_abs_diff = lambda li, v : sum([abs(i - v) for i in li])
-cum_abs_diff = lambda di, v : sum([abs(key - v) * value for key, value in di.items()])
+cum_abs_diff = lambda li, v : sum([abs(i - v) for i in li])
 
 if __name__ == "__main__":
     # input is from STDIN, and output to STDOUT
     N, M = map(int, input().split())
-    A = (list(map(int, input().split())))
+    A = list(map(int, input().split()))
     Q = list(map(int, input().split()))
-
-    # create a counter dictionary from A
-    A = counter(A)
 
     print(" ".join([str(cum_abs_diff(A, q)) for q in Q]))
